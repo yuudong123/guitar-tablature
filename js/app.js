7cd5a83c9a65044890978b8b9d23c42d02fdd3bb
yuudong123 <1mouri81@gmail.com>
2023-03-13 09:28:05 +0900

    js event delegation

--- js/app.js
+++ js/app.js
@@ -1,90 +1,101 @@
-const showButton = document.querySelector(".show-btn");
-const addLineButton = document.querySelector(".add-line-btn");
-const resetButton = document.querySelector(".reset-btn");
+// const showButton = document.querySelector(".show-btn");
+// const addLineButton = document.querySelector(".add-line-btn");
+// const resetButton = document.querySelector(".reset-btn");
+
+const Buttons = document.querySelector(".buttons");
 const saveButton = document.querySelector(".save-btn");
+const Lines = document.getElementById("lines");
 const chordTableContainer = document.getElementById("chord-table-container");
 const imageContainer = document.getElementById("tablature-image-container");
 var cntLine = 1;
 
-showButton.addEventListener("click", function () {
-  imageContainer.innerHTML = "";
-  const chordLines = document.querySelectorAll(
-    "#chord-table-container .chord-line"
-  );
+Buttons.addEventListener("click", (event) => {
+  const cls = event.target.className;
 
-  for (let i = 0; i < cntLine; i++) {
-    if (i != 0) {
+  switch (cls) {
+    case "show-btn": {
+      imageContainer.innerHTML = "";
+      const chordLines = document.querySelectorAll(
+        "#chord-table-container .chord-line"
+      );
+
+      for (let i = 0; i < cntLine; i++) {
+        if (i != 0) {
+          const newDividerLine = document.createElement("div");
+          newDividerLine.classList.add("divider-line");
+          imageContainer.appendChild(newDividerLine);
+        }
+
+        const currentChordLine = chordLines[i];
+        const newImageLine = document.createElement("div");
+        newImageLine.classList.add("image-line");
+
+        for (let j = 0; j < 8; j++) {
+          const currentChordInput = currentChordLine.children[j];
+          const chordInputValue = currentChordInput.value;
+
+          const chordImage = document.createElement("img");
+          chordImage.classList.add("chord-image");
+          if (chordInputValue == "") {
+            chordImage.src = "images/none.png";
+          } else {
+            chordImage.src = "images/" + chordInputValue + ".png";
+          }
+
+          newImageLine.appendChild(chordImage);
+        }
+
+        imageContainer.appendChild(newImageLine);
+      }
+      break;
+    }
+    case "add-line-btn": {
       const newDividerLine = document.createElement("div");
       newDividerLine.classList.add("divider-line");
-      imageContainer.appendChild(newDividerLine);
-    }
+      const newChordLine = document.createElement("div");
+      newChordLine.classList.add("chord-line");
 
-    const currentChordLine = chordLines[i];
-    const newImageLine = document.createElement("div");
-    newImageLine.classList.add("image-line");
+      chordTableContainer.appendChild(newDividerLine);
 
-    for (let j = 0; j < 8; j++) {
-      const currentChordInput = currentChordLine.children[j];
-      const chordInputValue = currentChordInput.value;
-
-      const chordImage = document.createElement("img");
-      chordImage.classList.add("chord-image");
-      if (chordInputValue == "") {
-        chordImage.src = "images/none.png";
-      } else {
-        chordImage.src = "images/" + chordInputValue + ".png";
+      for (let i = 0; i < 8; i++) {
+        const newChordInput = document.createElement("input");
+        newChordInput.type = "text";
+        newChordInput.classList.add("chord-input");
+        newChordLine.appendChild(newChordInput);
       }
 
-      newImageLine.appendChild(chordImage);
+      chordTableContainer.appendChild(newChordLine);
+      cntLine++;
+      Lines.innerText = cntLine;
+      break;
     }
+    case "reset-btn": {
+      chordTableContainer.innerHTML = "";
+      imageContainer.innerHTML = "";
+      const newChordLine = document.createElement("div");
+      newChordLine.classList.add("chord-line");
 
-    imageContainer.appendChild(newImageLine);
+      for (let i = 0; i < 8; i++) {
+        const newChordInput = document.createElement("input");
+        newChordInput.type = "text";
+        newChordInput.classList.add("chord-input");
+        newChordLine.appendChild(newChordInput);
+      }
+
+      chordTableContainer.appendChild(newChordLine);
+      cntLine = 1;
+      break;
+    }
   }
-});
-
-addLineButton.addEventListener("click", function () {
-  const newDividerLine = document.createElement("div");
-  newDividerLine.classList.add("divider-line");
-  const newChordLine = document.createElement("div");
-  newChordLine.classList.add("chord-line");
-
-  chordTableContainer.appendChild(newDividerLine);
-
-  for (let i = 0; i < 8; i++) {
-    const newChordInput = document.createElement("input");
-    newChordInput.type = "text";
-    newChordInput.classList.add("chord-input");
-    newChordLine.appendChild(newChordInput);
-  }
-
-  chordTableContainer.appendChild(newChordLine);
-  cntLine++;
-});
-
-resetButton.addEventListener("click", function () {
-  chordTableContainer.innerHTML = "";
-  imageContainer.innerHTML = "";
-  const newChordLine = document.createElement("div");
-  newChordLine.classList.add("chord-line");
-
-  for (let i = 0; i < 8; i++) {
-    const newChordInput = document.createElement("input");
-    newChordInput.type = "text";
-    newChordInput.classList.add("chord-input");
-    newChordLine.appendChild(newChordInput);
-  }
-
-  chordTableContainer.appendChild(newChordLine);
-  cntLine = 1;
 });
 
 saveButton.addEventListener("click", function () {
   html2canvas(document.getElementById("tablature-image-container")).then(
     function (canvas) {
-      var el = document.createElement("a");
-      el.href = canvas.toDataURL("image/png");
-      el.download = "New Tablature.png"; //다운로드 할 파일명 설정
-      el.click();
+      var a = document.createElement("a");
+      a.href = canvas.toDataURL("image/png");
+      a.download = "New Tablature.png";
+      a.click();
     }
   );
 });
