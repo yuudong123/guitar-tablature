274eee71d1292008a46355e230f6585dddb9baa6
yuudong123 <1mouri81@gmail.com>
2023-03-14 17:10:47 +0900

    script to JQuery

--- js/app.js
+++ js/app.js
@@ -1,100 +1,86 @@
-// const showButton = document.querySelector(".show-btn");
-// const addLineButton = document.querySelector(".add-line-btn");
-// const resetButton = document.querySelector(".reset-btn");
+$(document).ready(function () {
+  const Buttons = $(".buttons");
+  const saveButton = $(".save-btn");
+  const Lines = $("#lines");
+  const chordTableContainer = $("#chord-table-container");
+  const imageContainer = $("#tablature-image-container");
+  var cntLine = 1;
 
-const Buttons = document.querySelector(".buttons");
-const saveButton = document.querySelector(".save-btn");
-const Lines = document.getElementById("lines");
-const chordTableContainer = document.getElementById("chord-table-container");
-const imageContainer = document.getElementById("tablature-image-container");
-var cntLine = 1;
+  Buttons.on("click", (event) => {
+    const cls = event.target.className;
 
-Buttons.addEventListener("click", (event) => {
-  const cls = event.target.className;
+    switch (cls) {
+      case "show-btn": {
+        imageContainer.html("");
+        const chordLines = $("#chord-table-container .chord-line");
 
-  switch (cls) {
-    case "show-btn": {
-      imageContainer.innerHTML = "";
-      const chordLines = document.querySelectorAll(
-        "#chord-table-container .chord-line"
-      );
-
-      for (let i = 0; i < cntLine; i++) {
-        if (i != 0) {
-          const newDividerLine = document.createElement("div");
-          newDividerLine.classList.add("divider-line");
-          imageContainer.appendChild(newDividerLine);
-        }
-
-        const currentChordLine = chordLines[i];
-        const newImageLine = document.createElement("div");
-        newImageLine.classList.add("image-line");
-
-        for (let j = 0; j < 8; j++) {
-          const currentChordInput = currentChordLine.children[j];
-          const chordInputValue = currentChordInput.value;
-
-          const chordImage = document.createElement("img");
-          chordImage.classList.add("chord-image");
-          if (chordInputValue == "") {
-            chordImage.src = "images/none.png";
-          } else {
-            chordImage.src = "images/" + chordInputValue + ".png";
+        for (let i = 0; i < cntLine; i++) {
+          if (i != 0) {
+            const newDividerLine = $("<div class='divider-line'></div>");
+            imageContainer.append(newDividerLine);
           }
-          newImageLine.appendChild(chordImage);
+
+          const currentChordLine = chordLines.eq(i);
+          const newImageLine = $("<div class='image-line'></div>");
+
+          for (let j = 0; j < 8; j++) {
+            const currentChordInput = currentChordLine.children().eq(j);
+            const chordInputValue = currentChordInput.val();
+
+            const chordImage = $("<img class='chord-image'/>");
+            if (chordInputValue == "") {
+              chordImage.attr("src", "images/none.png");
+            } else {
+              chordImage.attr("src", "images/" + chordInputValue + ".png");
+            }
+            newImageLine.append(chordImage);
+          }
+
+          imageContainer.append(newImageLine);
+        }
+        break;
+      }
+      case "add-line-btn": {
+        const newDividerLine = $("<div class='divider-line'></div>");
+        const newChordLine = $("<div class='chord-line'></div>");
+
+        chordTableContainer.append(newDividerLine);
+
+        for (let i = 0; i < 8; i++) {
+          const newChordInput = $("<input type='text' class='chord-input'/>");
+          newChordLine.append(newChordInput);
         }
 
-        imageContainer.appendChild(newImageLine);
+        chordTableContainer.append(newChordLine);
+        cntLine++;
+        Lines.text(cntLine);
+        break;
       }
-      break;
-    }
-    case "add-line-btn": {
-      const newDividerLine = document.createElement("div");
-      newDividerLine.classList.add("divider-line");
-      const newChordLine = document.createElement("div");
-      newChordLine.classList.add("chord-line");
+      case "reset-btn": {
+        chordTableContainer.html("");
+        imageContainer.html("");
+        const newChordLine = $("<div class='chord-line'></div>");
 
-      chordTableContainer.appendChild(newDividerLine);
+        for (let i = 0; i < 8; i++) {
+          const newChordInput = $("<input type='text' class='chord-input'/>");
+          newChordLine.append(newChordInput);
+        }
 
-      for (let i = 0; i < 8; i++) {
-        const newChordInput = document.createElement("input");
-        newChordInput.type = "text";
-        newChordInput.classList.add("chord-input");
-        newChordLine.appendChild(newChordInput);
+        chordTableContainer.append(newChordLine);
+        cntLine = 1;
+        break;
       }
-
-      chordTableContainer.appendChild(newChordLine);
-      cntLine++;
-      Lines.innerText = cntLine;
-      break;
     }
-    case "reset-btn": {
-      chordTableContainer.innerHTML = "";
-      imageContainer.innerHTML = "";
-      const newChordLine = document.createElement("div");
-      newChordLine.classList.add("chord-line");
+  });
 
-      for (let i = 0; i < 8; i++) {
-        const newChordInput = document.createElement("input");
-        newChordInput.type = "text";
-        newChordInput.classList.add("chord-input");
-        newChordLine.appendChild(newChordInput);
+  saveButton.on("click", function () {
+    html2canvas(document.getElementById("tablature-image-container")).then(
+      function (canvas) {
+        var a = document.createElement("a");
+        a.href = canvas.toDataURL("image/png");
+        a.download = "New Tablature.png";
+        a.click();
       }
-
-      chordTableContainer.appendChild(newChordLine);
-      cntLine = 1;
-      break;
-    }
-  }
-});
-
-saveButton.addEventListener("click", function () {
-  html2canvas(document.getElementById("tablature-image-container")).then(
-    function (canvas) {
-      var a = document.createElement("a");
-      a.href = canvas.toDataURL("image/png");
-      a.download = "New Tablature.png";
-      a.click();
-    }
-  );
+    );
+  });
 });
